8067c7b47497b068ec77b379bb240ab161500c2a
erikefernandes40 <erikefernandes40@gmail.com>
2023-04-21 19:18:56 -0300

    feat: cria arquivo brideAndGroom.tsx

---
+++ pages/brideAndGroom.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import styles from '../styles/brideAndBroom.module.css'
+
+
+const BrideAndGroom = () => {
+  return (
+    <section className={styles.noivos}>
+      <div className={styles.titleNoivos}>
+        <h1>Os Noivos</h1>
+      </div>
+      <div className={styles.aee}>
+        <h1>Erick</h1>
+        <Image
+          src="/images/IMG-20230122-WA0020.jpg"
+          alt="Foto de Alana e Erick"
+          width={300}
+          height={300}
+          className={styles.eea}
+        />
+        <h1>Alana</h1>
+      </div>
+      <div className={styles.textAbout}>
+        <p>Criamos esse site para compartilhar com vocês os detalhes do nosso casamento. Estamos muito felizes e contamos com a presença de todos no nosso grande dia!
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default BrideAndGroom
