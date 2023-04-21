b9c6ba462107667f0a255596efba035ca7c486af
erikefernandes40 <erikefernandes40@gmail.com>
2023-04-21 16:31:17 -0300

    feat: cria aruivo background.tsx

---
+++ pages/backgound.tsx
@@ -0,0 +1,14 @@
+import styles from 'styles/backgound.module.css'
+
+const Background = () => {
+  return (
+    <section className={styles.background}>
+      <div className={styles.imagemPrincipal}>
+        <h1>Alana & Erick</h1>
+        <h3>14/03/2023</h3>
+      </div>
+    </section>
+  )
+}
+
+export default Background
