3723f2e80102c68c81ac48ef2a44caa8d7cd36a2
erikefernandes40 <erikefernandes40@gmail.com>
2023-04-21 16:37:26 -0300

    feat: retira partes do retorno para serem separados em outros arquivos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,17 @@
 import Image from 'next/image'
 import Formulario from './form'
+import Header from './header'
+import Background from './backgound'
+import Stopwatch from './stopwatch'
 export default function Home() {
   return (
     <>
-      <header>
-        <div className='logo'>
-          <h1>
-            A & E
-          </h1>
-        </div>
-        <div className='menu'>
-          <img src="https://cdn-icons-png.flaticon.com/512/3502/3502458.png" alt="menu" />
-        </div>
-      </header>
 
-      <section className='background'>
-        <div className="imagem-principal">
-          <h1>Alana & Erick</h1>
-          <h3>14/03/2023</h3>
-        </div>
-      </section>
+    <Header />
 
-      <section className="stopwatch">
-        <div className="regressive-count-text">
-          <h3>CONTAGEM REGRESSIVA PARA O GRANDE DIA</h3>
-        </div>
-        <div className="count-regressive-days">
-          <h3>200</h3>
-          <h3>09</h3>
-          <h3>15</h3>
-        </div>
-        <div className="time">
-          <h3>DIAS</h3>
-          <h3>HORAS</h3>
-          <h3>MINUTOS</h3>
-        </div>
-      </section>
+    <Background/>
+
+    <Stopwatch/>
 
       <section className="noivos">
         <div className="title-noivos">
