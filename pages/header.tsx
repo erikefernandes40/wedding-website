cc7f524a442401fa038ab04a4f200d25bd472299
erikefernandes40 <erikefernandes40@gmail.com>
2023-04-21 16:36:48 -0300

    feat: adiciona menu ao header

--- pages/header.tsx
+++ pages/header.tsx
@@ -1,20 +1,59 @@
+import Link from 'next/link';
+import { useState } from 'react';
 import styles from 'styles/header.module.css';
 
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className={styles.header}>
-      <div>
-        <h1>
-          A & E
-        </h1>
+      <div className={styles.logo}>
+        <Link href="/">
+          A&E
+        </Link>
       </div>
 
-      <div>
-        <img className={styles.headerImg} src="https://cdn-icons-png.flaticon.com/512/3502/3502458.png" alt="menu" />
-      </div>
+      <button className={styles.menuIcon} onClick={handleToggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <nav className={`${styles.menu} ${isOpen ? styles.open : ''}`}>
+        <ul>
+          <li>
+            <Link href="/">
+              Os Noivos
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              Os Padrinhos
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              Lista de Presentes
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              Cerimônia
+            </Link>
+          </li>
+          <li>
+            <Link href="/contato">
+              Deixe seu Recado
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-
   )
 }
 
