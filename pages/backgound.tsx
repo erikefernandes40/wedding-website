b2c1d88c036c2bd6a72d8bbc4bc6407df6277c70
erikefernandes40 <erikefernandes40@gmail.com>
2023-06-25 08:20:55 -0300

    feat: adiciona fotos acima da imagem principal

--- pages/backgound.tsx
+++ pages/backgound.tsx
@@ -1,11 +1,34 @@
 import styles from 'styles/backgound.module.css'
+import Image from 'next/image'
+
 
 const Background = () => {
   return (
     <section className={styles.background}>
       <div className={styles.imagemPrincipal}>
-        <h1>Alana & Erick</h1>
-        <h3>14/03/2023</h3>
+        <div>
+          <Image
+            src="/images/line_top.png"
+            alt="Foto de Alana e Erick"
+            width={250}
+            height={40}
+            className={styles.lineTop}
+          />
+        </div>
+
+        <div>
+          <h1>Alana & Erick</h1>
+        </div>
+
+        <div>
+          <Image
+            src="/images/line_bottom.png"
+            alt="Foto de Alana e Erick"
+            width={250}
+            height={40}
+            className={styles.lineBottom}
+          />
+        </div>
       </div>
     </section>
   )
