d84a751caf16d1915d478d73fe1bf8daf677981a
erikefernandes40 <erikefernandes40@gmail.com>
2023-06-25 08:25:10 -0300

    refactor: retira sessões para passar para outro arquivo

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,28 +8,13 @@ import PresentList from './presentList'
 export default function Home() {
   return (
     <>
-    <Header />
-    <Background/>
-    <Stopwatch/>
-    <BrideAndGroom/>
-    <GodParents/>
-    <PresentList/>
-
-
-
-
-
-      <section className='cerimonia'>
-        <h1>Cerimônia</h1>
-        <p>Nossa Cerimônia será no dia 14/10/2023, na Igreja Presbiteriana do Bairro Vila Rubens, começará às 17h.
-        </p>
-
-        <h2>Deixe seu recado para os noivos</h2>
-
-        <Formulario />
-
-      </section>
-
+      <Header />
+      <Background />
+      <Stopwatch />
+      <BrideAndGroom />
+      <GodParents />
+      <PresentList />
+      <Formulario />
     </>
 
   )
