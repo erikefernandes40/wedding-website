21a6df145979b336e8b94faddc8566f2f041403c
erikefernandes40 <erikefernandes40@gmail.com>
2023-07-23 10:52:48 -0300

    feat: adiciona arquivo de configuração _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='pt-br'>
+        <Head>
+          <meta content='width=device-width, initial-scale=1.0' name='viewport' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
