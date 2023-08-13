a80b7c4d3264721c7c36ff36870b8961e999027f
erikefernandes40 <erikefernandes40@gmail.com>
2023-08-13 18:48:30 -0300

    feat: adiciona foto aos noivos

--- pages/brideAndGroom.tsx
+++ pages/brideAndGroom.tsx
@@ -9,7 +9,6 @@ const BrideAndGroom = () => {
         <h1>Os Noivos</h1>
       </div>
       <div className={styles.aee}>
-        <h1>Erick</h1>
         <Image
           src="/images/IMG-20230122-WA0020.jpg"
           alt="Foto de Alana e Erick"
@@ -17,7 +16,19 @@ const BrideAndGroom = () => {
           height={300}
           className={styles.eea}
         />
+        <div className={styles.names}>
+        <h1>Erick</h1>
+        <h1>&</h1>
         <h1>Alana</h1>
+        </div>
+
+        <Image
+          src="/images/IMG-20230122-WA0020.jpg"
+          alt="Foto de Alana e Erick"
+          width={300}
+          height={300}
+          className={styles.eea}
+        />
       </div>
       <div className={styles.textAbout}>
         <p>Criamos esse site para compartilhar com vocês os detalhes do nosso casamento. Estamos muito felizes e contamos com a presença de todos no nosso grande dia!
